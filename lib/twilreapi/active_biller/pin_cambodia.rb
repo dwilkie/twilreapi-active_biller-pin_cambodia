bab1448914956e0153a12f6e3a1e70a90cc7966d
David Wilkie <dwilkie@gmail.com>
2016-11-30 15:54:04 +0700

    initial commit

---
+++ lib/twilreapi/active_biller/pin_cambodia.rb
@@ -0,0 +1,9 @@
+require "twilreapi/active_biller/pin_cambodia/version"
+
+module Twilreapi
+  module ActiveBiller
+    module PinCambodia
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/twilreapi/active_biller/pin_cambodia/version.rb
@@ -0,0 +1,7 @@
+module Twilreapi
+  module ActiveBiller
+    module PinCambodia
+      VERSION = "0.1.0"
+    end
+  end
+end
